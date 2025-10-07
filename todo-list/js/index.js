5851f4a0c816edeacbd7f67e9c2bf4a488a59c0e
Oleksandra <oleksandrashkutova@gmail.com>
2025-10-07 19:32:53 +0300

    Add ToDo Dom.

---
+++ todo-list/js/index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const todoForm = document.querySelector('.todoContainer>form')
+const todoInput = document.querySelector('.todoContainer input');
+const todoList = document.querySelector('.todoList');
+const TODO_REX_EXP = /^\s*$/;
+
+todoInput.oninput = ({ target }) => {
+  if (!TODO_REX_EXP.test(target.value)) {
+    target.classList.add('valid');
+    target.classList.remove('invalid');
+  } else {
+    target.classList.remove('valid');
+    target.classList.add('invalid');
+  }
+};
+
+todoForm.onsubmit = (e) => {
+    e.preventDefault();
+    const todoItem = e.target.elements['todo-item']
+    if (!TODO_REX_EXP.test(todoItem.value)) {
+        const todoListItem = createTodoItem(todoItem.value);
+        todoList.append(todoListItem);
+        todoItem.value = '';
+    } else {
+        todoItem.classList.add('invalid');
+    }
+}
+
+function createTodoItem (value) {
+    const todiListItem = document.createElement('li');
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'X';
+    delBtn.onclick = ({target}) => {
+        target.parentElement.remove();
+    }
+
+    const isDoneEl = document.createElement('input');
+    isDoneEl.type = 'checkbox';
+    isDoneEl.onchange = ({ target }) => {
+    target.nextSibling.classList.toggle('doneTask');
+    };
+
+    const todoValue = document.createElement('span');
+    todoValue.textContent = value;
+
+    todiListItem.append(delBtn, todoValue);
+    return todiListItem;
+}
